5150f2ea35c8eec43c30ae0bb0e1ad3ec1485afc
ilovetraffic <ilovetraffic@yandex.ru>
2018-11-15 17:41:37 +0300

    adchoise

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = (data) => {
 
     let hotJarString = `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1081613,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
     let adChoise = `var _qevents=_qevents || [];(function(){var elem=document.createElement('script');elem.src=(document.location.protocol=="https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";elem.async=true;elem.type="text/javascript";var scpt=document.getElementsByTagName('script')[0];scpt.parentNode.insertBefore(elem, scpt);})();_qevents.push({qacct:"p-LTcxNEKxGqzjz"});`
-    let adChoiseNoScript = `<div style="display:none;"><img src="//pixel.quantserve.com/pixel/p-LTcxNEKxGqzjz.gif" border="0" height="1" width="1" alt="Quantcast"/></div>`
+   
   
     return (
         <div>
@@ -20,7 +20,9 @@ const Header = (data) => {
             <meta name="google-site-verification" content="nhP1DAMDbhzaxvNg1ITUGaOtdvZ2AyIwJQrCoOOo09Q" />
             <script>{hotJarString}</script>
             <noscript>
-                {adChoiseNoScript}
+            <div style="display:none;">
+            <img src="//pixel.quantserve.com/pixel/p-LTcxNEKxGqzjz.gif" border="0" height="1" width="1" alt="Quantcast"/>
+            </div>
             </noscript>
             <script>{adChoise}</script>
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
